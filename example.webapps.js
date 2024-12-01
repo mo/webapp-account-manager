9150a4e47aa5c45c3860a0b9bb6a3b2a8c82c9ec
Martin Olsson <martin@minimum.se>
2024-12-01 15:59:28 +0100

    Add OWASP Juice Shop as example webapp

--- example.webapps.js
+++ example.webapps.js
@@ -42,4 +42,51 @@ export default [
       await page.waitForLoadState('networkidle')
     },
   },
+  {
+    appid: 'owasp-juice',
+    accounts: [
+      {
+        userid: 'a@a.com',
+        passwd: 'Juicer4!',
+      },
+      {
+        userid: 'b@b.com',
+        passwd: 'Juicer4!',
+      },
+    ],
+    sessionCredentials: [
+      { type: 'authorization', subtype: 'Bearer', maxAge: 1500 },
+    ],
+    login: async (page, userid, passwd) => {
+      await page.goto('https://demo.owasp-juice.shop/#/login')
+      // Dismiss welcome to Juice shop modal
+      await page.getByLabel('Close Welcome Banner').click()
+
+      await page.click('#email')
+      await page.keyboard.type(userid)
+      await page.click('#password')
+      await page.keyboard.type(passwd)
+      await page.getByLabel('Login', { exact: true }).click()
+    },
+    register: async (page, userid, passwd) => {
+      await page.goto('https://demo.owasp-juice.shop/#/register')
+      // Dismiss welcome to Juice shop modal
+      await page.getByLabel('Close Welcome Banner').click()
+
+      await page.getByLabel('Email address field').click()
+      await page.keyboard.type(userid)
+      await page.getByLabel('Field for the password').click()
+      await page.keyboard.type(passwd)
+      await page.getByLabel('Field to confirm the password').click()
+      await page.keyboard.type(passwd)
+      await page.click('[name="securityQuestion"]')
+      await page.click('#mat-option-0')
+
+      await page.getByLabel('Field for the answer to the security question')
+        .click()
+      await page.keyboard.type('a')
+
+      await page.getByLabel('Button to complete the registration').click()
+    },
+  },
 ]
